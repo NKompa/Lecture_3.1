f693277f63dd196eccc634ddc34ea22e1b315fff
NKompa <nkompa7@gmail.com>
2022-08-21 00:26:41 +0300

    Example 3.1 Sum Random

---
+++ Example03.1_SumRandom/Program.cs
@@ -0,0 +1,5 @@
+﻿int numberA=new Random().Next(1,10); // 1 2 3 ... 9
+int numberB=new Random().Next(1,10);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA+numberB);
