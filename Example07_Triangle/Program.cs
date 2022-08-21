175461ca8e790179e7ba56589ab53d31012ddf34
NKompa <nkompa7@gmail.com>
2022-08-21 19:47:40 +0300

    Some modifications

--- Example07_Triangle/Program.cs
+++ Example07_Triangle/Program.cs
@@ -4,38 +4,38 @@ int xa = 40, ya = 1,
     xb = 1, yb = 30,
     xc = 80, yc = 30;
 
-Console.SetCursorPosition(xa,ya);
+Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
 
-Console.SetCursorPosition(xb,yb);
+Console.SetCursorPosition(xb, yb);
 Console.WriteLine("+");
 
-Console.SetCursorPosition(xc,yc);
+Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
-int x = xa, y = xb;
+int x = xa, y = ya;
 
 int count = 0;
 
 while (count < 10000)
 {
-    int what = new Random().Next(0,3); // 0 1 2
-    if(what == 0)
+    int what = new Random().Next(0, 3); // 0 1 2
+    if (what == 0)
     {
-        x = (x+xa)/2;
-        y = (y+ya)/2;
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
     }
-    if(what == 1)
+    if (what == 1)
     {
-        x = (x+xb)/2;
-        y = (y+yb)/2;
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
     }
-    if(what == 2)
+    if (what == 2)
     {
-        x = (x+xc)/2;
-        y = (y+yc)/2;
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
     }
-Console.SetCursorPosition(x,y);
-Console.WriteLine("+");
-count++;
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
 }
